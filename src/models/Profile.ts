696ce278275c00add830e028ccfe281c5f9e973f
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-14 02:46:50 -0300

    feat: Actualizar modelo de Profile

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -1,11 +1,18 @@
 import type { Languages } from "./Languages"
 import type { SocialMedia } from "./SocialMedia"
 
+interface Skills {
+    "front": string[],
+    "back": string[],
+    "tools": string[],
+    "learning": string[]
+}
+
 export interface Profile {
     "firstName": string,
     "lastName": string,
     "bio": Record<Languages, string>,
-    "stack": string[],
+    "skills": Skills,
     "email":string,
     "socialMedia": SocialMedia[],
     "CV":Record<Languages, string>
