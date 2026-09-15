e97251741445fa6a4303a9e88f316add30cbbe3f
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-15 11:24:29 -0300

    feat: Separar texto de AboutMe del componente sección

---
+++ src/features/01 - AboutMe/aboutMe.tsx
@@ -0,0 +1,26 @@
+import type { Profile } from "../../models/Profile";
+import type { Languages } from "../../models/Languages";
+import { translateHeader } from "./translateHeader";
+import { CVButton } from "../../shared/CVButton";
+import styles from "./aboutMe.module.css"
+
+const AboutMeContainer = ({profileData, lan}:{profileData:Profile, lan:Languages}) => {
+    return <div className={styles.aboutMeContainer}>
+        <div>
+            <h3>{translateHeader(lan, "AboutMe")}</h3>
+            <p>{profileData.bio[lan].map((e:string)=>{
+                return <p>{e}</p>
+            })}</p>
+        </div>
+        <div>
+            <h3>{translateHeader(lan, "Languages")}</h3>
+            {profileData.languages[lan].map((e:string)=>{
+                return <p>{e}</p>})}
+        </div>
+        <div>
+            <CVButton profileData={profileData} lan={lan}/>
+        </div>
+    </div>
+}
+
+export default AboutMeContainer;
