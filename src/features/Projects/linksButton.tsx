35bc79100084fd0caa11f7f74056883e257bf734
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-15 01:57:04 -0300

    feat: Cambiar icono de páginas

--- src/features/Projects/linksButton.tsx
+++ src/features/Projects/linksButton.tsx
@@ -1,12 +1,12 @@
 const LinkRepo = ({url}:{url:string|undefined}) => {
     return url? <a href={url} target="_blank" rel="noopener noreferrer" title="Entrar a Repositorio">
-        <img src="/icons/github.webp" height="40px" alt ="Github icon"/>
+        <img src="/icons/github.webp" height="50px" alt ="Github icon"/>
     </a> : null;
 }
 
 const LinkPage = ({url}:{url:string|undefined}) => {
     return url? <a href={url} target="_blank" rel="noopener noreferrer" title="Entrar a Página">
-        <img src="/icons/mozilla.webp" height="40px" alt="Mozilla icon"/>
+        <img src="/icons/page.webp" height="50px" alt="Mozilla icon"/>
     </a> : null;
 }
 
