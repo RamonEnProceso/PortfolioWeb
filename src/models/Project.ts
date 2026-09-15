692ca4589407547417f0d414ccea861998332e1a
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-15 00:52:12 -0300

    fix: Arreglar modelo Project

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -3,7 +3,7 @@ import type { Languages } from "./Languages"
 export interface Project {
     "name":string,
     "url"?: string,
-    "repo":string,
+    "repo"?:string,
     "thumbnail": string,
     "description": Record<Languages, string>,
     "stack": string[],
