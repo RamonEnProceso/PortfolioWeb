02a4f5befbdf504d67b9f696daea8c858c168c0c
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-15 13:36:43 -0300

    fix: Arreglar display de imagenes

--- src/features/03 - Projects/linksButton.tsx
+++ src/features/03 - Projects/linksButton.tsx
@@ -2,13 +2,13 @@ import styles from "./linksButton.module.css"
 
 const LinkRepo = ({url}:{url:string|undefined}) => {
     return url? <a href={url} target="_blank" rel="noopener noreferrer" title="Entrar a Repositorio">
-        <img src="/icons/github.webp" height="50px" alt ="Github icon"/>
+        <img src="icons/github.webp" height="50px" alt ="Github icon"/>
     </a> : null;
 }
 
 const LinkPage = ({url}:{url:string|undefined}) => {
     return url? <a href={url} target="_blank" rel="noopener noreferrer" title="Entrar a Página">
-        <img src="/icons/page.webp" height="50px" alt="Mozilla icon"/>
+        <img src="icons/page.webp" height="50px" alt="Mozilla icon"/>
     </a> : null;
 }
 
