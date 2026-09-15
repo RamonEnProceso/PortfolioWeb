1859f9d2915268b48495a2ccc35e0314a4648197
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-15 11:45:32 -0300

    style: Centrar componentes

--- src/features/01 - AboutMe/profileHeader.tsx
+++ src/features/01 - AboutMe/profileHeader.tsx
@@ -1,30 +1,17 @@
 import type { Profile } from "../../models/Profile";
 import type { Languages } from "../../models/Languages";
-import { translateHeader } from "./translateHeader";
-import { CVButton } from "../../shared/CVButton";
 import SkillsContainer from "../02 - Skills/skillsContainer";
+import AboutMeContainer from "./aboutMe";
+import styles from "./profileHeader.module.css"
 
 const ProfileHeader = ({profileData, lan}:{profileData:Profile, lan:Languages}) => {
     
-    return <>
-    <section>
-        <div>
-            <h3>{translateHeader(lan, "AboutMe")}</h3>
-            <p>{profileData.bio[lan].map((e:string)=>{
-                return <p>{e}</p>
-            })}</p>
-        </div>
-        <div>
-            <h3>{translateHeader(lan, "Languages")}</h3>
-            {profileData.languages[lan].map((e:string)=>{
-                return <p>{e}</p>})}
-        </div>
+    return <section className={styles.aboutMeContainer}>
+    <div className={styles.aboutMeDiv}>
+        <AboutMeContainer profileData={profileData} lan={lan}/>
         <SkillsContainer profileData={profileData} lan={lan}/>
-        <div>
-            <CVButton profileData={profileData} lan={lan}/>
-        </div>
+    </div>
     </section>
-    </>
 }
 
 export default ProfileHeader;
