c75b7f7fd6521a0322ef85aefde381fee923cbcc
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-15 01:37:12 -0300

    fix: Arreglar imágenes y agregar title

--- src/features/Projects/linksButton.tsx
+++ src/features/Projects/linksButton.tsx
@@ -1,14 +1,12 @@
-import IconDisplay from "../../shared/iconDisplay"
-
 const LinkRepo = ({url}:{url:string|undefined}) => {
-    return url? <a href={url} target="_blank" rel="noopener noreferrer">
-        <IconDisplay name={"Github"}/>
+    return url? <a href={url} target="_blank" rel="noopener noreferrer" title="Entrar a Repositorio">
+        <img src="/icons/github.webp" height="40px" alt ="Github icon"/>
     </a> : null;
 }
 
 const LinkPage = ({url}:{url:string|undefined}) => {
-    return url? <a href={url} target="_blank" rel="noopener noreferrer">
-        <IconDisplay name={"page"}/>
+    return url? <a href={url} target="_blank" rel="noopener noreferrer" title="Entrar a Página">
+        <img src="/icons/mozilla.webp" height="40px" alt="Mozilla icon"/>
     </a> : null;
 }
 
