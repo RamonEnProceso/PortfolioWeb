8cd335f177455196b7432f5ba2f07369811bca8b
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-16 08:27:45 -0300

    style: Mejorar interfaz del menu

--- src/features/Menu/portfolioNav.tsx
+++ src/features/Menu/portfolioNav.tsx
@@ -1,17 +1,18 @@
 import type { PageData } from "../../models/PageData"
 import TranslateButton from "./translateButton"
 import { translateMenu } from "./translateMenu"
+import styles from "./portfolioNav.module.css"
 
 const Menu = ({data}:{data:PageData}) => {
     return (
-        <nav>
-            <div>
+        <nav className={styles.menu}>
+            <div className={styles.index}>
             <a href="#home">{translateMenu(data.lan,"Home")}</a>
             <a href="#about">{translateMenu(data.lan,"About")}</a>
             <a href="#projects">{translateMenu(data.lan,"Projects")}</a>
             <a href="#contact">{translateMenu(data.lan,"Contact")}</a>
             </div>
-            <div>
+            <div className={styles.buttons}>
                 <TranslateButton lan={data.lan} setLan={data.setLan}></TranslateButton>
             </div>
         </nav>
