0f37bf41c74cacb1b126f27a328fe5daae2d304a
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-16 06:02:21 -0300

    fix: Mejorar función, eliminando switch anidado

--- src/features/01 - AboutMe/translateHeader.ts
+++ src/features/01 - AboutMe/translateHeader.ts
@@ -2,36 +2,22 @@ import type { Languages } from "../../models/Languages";
 
 type Header = "AboutMe"| "Languages" | "Skills" | "Tools";
 
-export const translateHeader = (tipo : Languages, header:Header) => {
-    switch(header){
-        case "AboutMe":
-            switch(tipo){
-                case "EN":
-                    return "About Me";
-                case "ES":
-                    return "Sobre mí";
-            };
-        case "Languages":
-            switch(tipo){
-                case "EN":
-                    return "Languages";
-                case "ES":
-                    return "Idiomas";
-            }
-        case "Skills":
-            switch(tipo){
-                case "EN":
-                    return "Skills"
-                case "ES":
-                    return "Habilidades"
-            }
-        case "Tools":
-            switch(tipo){
-                case "EN":
-                    return "Tools"
-                case "ES":
-                    return "Herramientas"
-            }
-
+export const translateHeader = (lan : Languages, header:Header) => {
+    const headerObj : Record <Header,Record<Languages,string>>= {
+        "AboutMe":{
+            "EN":"About Me",
+            "ES":"Sobre mí"},
+        "Languages":{
+            "EN":"Languages",
+            "ES":"Idiomas"},
+        "Skills":{
+            "EN":"Skills",
+            "ES":"Habilidades"},
+        "Tools":{
+            "EN":"Tools",
+            "ES":"Herramientas"
+        }
     }
+    return headerObj[header][lan];
 }
+    
