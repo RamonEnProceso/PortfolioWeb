09d88ff1a9cdd2e149db977f6db176cf7501ab6e
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-23 12:55:10 -0300

    feat: Crear Separador entre secciones

---
+++ src/shared/separator.tsx
@@ -0,0 +1,9 @@
+import styles from "./separator.module.css"
+
+const Separator = () => {
+    return <div className={styles.separatorBorder}>
+        <div className={styles.separator}/>
+    </div>
+}
+
+export default Separator;
